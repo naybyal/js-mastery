b7f52dbcbb4ac5c394c0f34702718eeeb48828e9
Nabiel <nabiel@localhost.localdomain>
2024-02-01 18:35:38 +0530

    2 hours of work...

--- 7 checked property/main.js
+++ 7 checked property/main.js
@@ -1,20 +1,32 @@
-/*
+var rootButton = document.getElementById('myButton');
+var rootCheckbox = document.getElementById('myCheckbox');
+var rootDescription = document.getElementById('myMessage');
+var rootPayInfo = document.getElementById('paymentInfo')
 
-let age= 21;
-console.log(age++);
-console.log(typeof NaN)
-console.log(typeof null)
-let firstName = "JSON";
-let student = true;
-console.log(age)
-console.log("Hello",firstName+"!")
-console.log(null === NaN)
 
-*/
+rootButton.onclick = () => {
+    const visaBtn = document.getElementById('visaBtn');
+    const upiBtn = document.getElementById('upiBtn');
+    const paypalBtn = document.getElementById('paypalBtn');
+    
+    if (rootCheckbox.checked && (visaBtn.checked || upiBtn.checked || paypalBtn.checked)) {
+        if (visaBtn.checked) {
+            rootPayInfo.textContent = 'Payment Method : Visa';
+        } else if (upiBtn.checked) {
+            rootPayInfo.textContent = 'Payment Method : UPI';
+        } else if (paypalBtn.checked) {
+            rootPayInfo.textContent = 'Payment Method : PayPal';
+        }
+        rootDescription.textContent = 'Congratulations! You are subscribed to our channel.';
+    }
+    else if (rootCheckbox.checked) {
+        rootDescription.textContent = 'Kindly choose a payment gateway.';
+        rootPayInfo.textContent = '';
+    } 
+    else {
+        rootDescription.textContent = 'Please subscribe to our channel. :(';
+        rootPayInfo.textContent = '';
+    }
 
-let firstName = "Nabiel";
-let student = true;
-let age = 22;
-document.getElementById('p1').innerHTML='Hello there, ' + firstName + '!';
-document.getElementById('p2').innerHTML='You are ' + age + ' years old.';
-document.getElementById('p3').innerHTML='Enrolled: ' + student;
+    
+}
