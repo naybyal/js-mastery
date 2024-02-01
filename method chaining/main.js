e106e86031c817b95ad4e292c4a56f6388d881ea
Nabiel <nabiel@localhost.localdomain>
2024-02-01 17:46:11 +0530

    2 hours of work...

--- method chaining/main.js
+++ method chaining/main.js
@@ -1,20 +1,4 @@
-/*
 
-let age= 21;
-console.log(age++);
-console.log(typeof NaN)
-console.log(typeof null)
-let firstName = "JSON";
-let student = true;
-console.log(age)
-console.log("Hello",firstName+"!")
-console.log(null === NaN)
-
-*/
-
-let firstName = "Nabiel";
-let student = true;
-let age = 22;
-document.getElementById('p1').innerHTML='Hello there, ' + firstName + '!';
-document.getElementById('p2').innerHTML='You are ' + age + ' years old.';
-document.getElementById('p3').innerHTML='Enrolled: ' + student;
+//  method chaining - calling one method after another in one continuous line of code
+let userName = '       roxxane      '
+let letter = userName.charAt(0).toUpperCase().trim();
