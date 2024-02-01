fd49da3d3f253bfee908dad11faf25b1f6d45683
Nabiel <nabiel@localhost.localdomain>
2024-02-01 14:24:58 +0530

    Starting out

---
+++ arithmetic expressions/main.js
@@ -0,0 +1,20 @@
+/*
+    arithmetic expression is a combination of operands and operators
+ */
+
+let students = 20;
+
+console.log(students%2);
+console.log(students%3);
+
+//  students++
+//  students--
+
+/* 
+    OPERATOR PRECEDENCE
+    1. paranthesis
+    2. exponents
+    3. multiplication and division
+    4. addition and subtraction
+*/
+
